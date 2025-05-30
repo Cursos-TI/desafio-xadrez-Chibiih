d563ffd7c5d7b272e47020305f50d93810949d35
Scheilla Vieira <scheilla.vet04@gmail.com>
2025-05-29 21:25:23 -0300

    nivel novato finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Desafio de Xadrez - MateCheck - Scheilla Vieira
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
@@ -27,6 +14,42 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    //Nivel novato
+    // Movendo a Torre
+    int torre = 5; // Número de casas a serem movidas
+    printf("Movimento da Torre:\n");
+
+    // Utilizando o for
+    for (int i = 1; i <= torre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // Movendo o Bispo
+    int bispo = 5; 
+    printf("Movimento do Bispo:\n");
+
+    // While
+    int contadorBispo = 1;
+    while (contadorBispo <= bispo) {
+        printf("Cima e direita\n");
+        contadorBispo++;
+    }
+
+    printf("\n");
+
+    // Movendo a Rainha
+    int rainha = 8; 
+    printf("Movimento da Rainha:\n");
+
+    // Do-while
+    int contadorRainha = 1;
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= rainha);
 
     return 0;
 }
